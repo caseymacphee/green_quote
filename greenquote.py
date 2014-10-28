2f3e8cd3bee5feff25eb454efdb4671ead35a917
Casey MacPhee <caseymacphee@gmail.com>
2014-10-28 15:49:58 -0700

    Ignore data base table creation for testing...

--- greenquote.py
+++ greenquote.py
@@ -15,16 +15,16 @@ engine = create_engine(app.config['DATABASE'])
 
 display_val = "Loading data..."
 
-def load_data():
-	dfdict = getqf.scraper()
-	df = dfdict['nsdqct.csv']
-	df.to_sql(name='entries', con = engine, if_exists = 'replace')
-	output = pd.read_sql_query('SELECT * FROM entries', engine)
-	mean = output[[2]].mean()
-	display_val = u"The mean is :" + str(mean)
+# def load_data():
+# 	dfdict = getqf.scraper()
+# 	df = dfdict['nsdqct.csv']
+# 	df.to_sql(name='entries', con = engine, if_exists = 'replace')
+# 	output = pd.read_sql_query('SELECT * FROM entries', engine)
+# 	mean = output[[2]].mean()
+# 	display_val = u"The mean is :" + str(mean)
 
-thread1 = Thread(target = load_data)
-thread1.start()
+# thread1 = Thread(target = load_data)
+# thread1.start()
 
 @app.route('/')
 def hello():
