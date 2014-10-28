442f7d10429ed6964a5558680780b3c79ae12d5b
Casey MacPhee <caseymacphee@gmail.com>
2014-10-28 15:17:27 -0700

    Fix problem with Heroku database url

--- greenquote.py
+++ greenquote.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from flask import Flask
 import pandas as pd
 sys.path.insert(0, "../financialScraper")
@@ -6,14 +7,15 @@ from financialScraper import getqf
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
-
+app.config['DATABASE'] = os.environ.get(
+	'HEROKU_POSTGRESQL_GOLD_URL', ''
+	)
+dfdict = getqf.scraper()
+engine = create_engine(app.config['DATABASE'])
 @app.route('/')
 def hello():
-	engine = create_engine('postgresql://postgres:becreative@localhost/greenq')
-	dfdict = getqf.scraper()
 	df = dfdict['nsdqct.csv']
 	df.to_sql(name='entries', con = engine, if_exists = 'replace')
-
 	output = pd.read_sql_query('SELECT * FROM entries', engine)
 	mean = output[[2]].mean()
 	return u"The mean is :" + str(mean)
