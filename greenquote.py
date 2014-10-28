7004b394867df2038279a582a0099eaef7dfae69
Casey MacPhee <caseymacphee@gmail.com>
2014-10-28 15:45:21 -0700

    Thread initial data loading.

--- greenquote.py
+++ greenquote.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from threading import Thread
 from flask import Flask
 import pandas as pd
 sys.path.insert(0, "../financialScraper")
@@ -10,15 +11,25 @@ app = Flask(__name__)
 app.config['DATABASE'] = os.environ.get(
 	'HEROKU_POSTGRESQL_GOLD_URL', ''
 	)
-dfdict = getqf.scraper()
 engine = create_engine(app.config['DATABASE'])
-@app.route('/')
-def hello():
+
+display_val = "Loading data..."
+
+def load_data():
+	dfdict = getqf.scraper()
 	df = dfdict['nsdqct.csv']
 	df.to_sql(name='entries', con = engine, if_exists = 'replace')
 	output = pd.read_sql_query('SELECT * FROM entries', engine)
 	mean = output[[2]].mean()
-	return u"The mean is :" + str(mean)
+	display_val = u"The mean is :" + str(mean)
+
+thread1 = Thread(target = load_data)
+thread1.start()
+
+@app.route('/')
+def hello():
+	return display_val
+	
 
 if __name__ == "__main__":
     app.run(debug=True)
