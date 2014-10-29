de0c2e88f16124804582334559ab45c85f5ee501
Casey MacPhee <caseymacphee@gmail.com>
2014-10-29 10:17:10 -0700

    Hardcode Heroku psql database url

--- backgroundworker.py
+++ backgroundworker.py
@@ -7,8 +7,9 @@ from sqlalchemy import create_engine
 
 running = True
 
-while running:
-	engine = create_engine(os.environ.get('DATABASE_URL'))
-	dfdict = getqf.scraper()
-	df = dfdict['nsdqct.csv']
-	df.to_sql(name='entries', con = engine, if_exists = 'replace')
+
+# engine = create_engine(os.environ.get('DATABASE_URL'))
+engine = create_engine('postgres://fkwfcpvbchmxps:VCmxue5WFWCOOHt56aqOm4FD_Z@ec2-54-83-205-46.compute-1.amazonaws.com:5432/d376d3nru8envq')
+dfdict = getqf.scraper()
+df = dfdict['nsdqct.csv']
+df.to_sql(name='entries', con = engine, if_exists = 'replace')
