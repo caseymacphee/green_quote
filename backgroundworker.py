f3ea9820a96536e74e6f74f13387140c97ea9f2e
Casey MacPhee <caseymacphee@gmail.com>
2014-10-29 10:33:35 -0700

    Add engine connection, and close engine connection to worker dyno

--- backgroundworker.py
+++ backgroundworker.py
@@ -9,7 +9,11 @@ running = True
 
 
 # engine = create_engine(os.environ.get('DATABASE_URL'))
+
 engine = create_engine('postgres://fkwfcpvbchmxps:VCmxue5WFWCOOHt56aqOm4FD_Z@ec2-54-83-205-46.compute-1.amazonaws.com:5432/d376d3nru8envq')
+connection = engine.connect()
 dfdict = getqf.scraper()
 df = dfdict['nsdqct.csv']
-df.to_sql(name='entries', con = engine, if_exists = 'replace')
+df.to_sql(name='entries', con = connection, if_exists = 'replace')
+
+connection.close()
